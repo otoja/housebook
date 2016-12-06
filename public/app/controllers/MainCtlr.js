cf2f8a48551a7e02234188ae821eaf6128d7df25
Kamila Przychodzeń <kamila.przychodzen@gmail.com>
2016-12-06 15:05:14 +0100

    Add contact form

--- public/app/controllers/MainCtlr.js
+++ public/app/controllers/MainCtlr.js
@@ -1,3 +1,18 @@
 housebook.controller('MainCtlr', function($scope, $rootScope){
+    $scope.formSent = false;
     
+    $scope.sentMail=function(name,email,message){
+        $.ajax({
+            url: "https://formspree.io/shagrin84@gmail.com",
+            method: "POST",
+            data: {message: message, _subject: "New contact form pilot application", replyTo: email},
+            dataType: "json"
+        }).then(function () {
+            _.defer(function(){
+                $scope.formSent = true;
+                $scope.$apply();
+            });
+            
+        });
+    };
 });
