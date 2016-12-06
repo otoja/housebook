921efc5b0a3ff17d5e9a4d5a9b3341e68eb9b92c
Kamila Przychodzeń <kamila.przychodzen@gmail.com>
2016-12-06 14:33:25 +0100

    login redirection

--- public/app/app.js
+++ public/app/app.js
@@ -2,13 +2,13 @@ var housebook = angular.module('housebook', ['ngRoute']);
 
 housebook.config(function ($routeProvider) {
     $routeProvider
-            .when('/', {templateUrl: 'partials/start.html'})
+            .when('/welcome', {templateUrl: 'partials/start.html'})
             .when('/register', {controller: 'RegisterCtlr', templateUrl: 'partials/register.html'})
             .when('/login', {controller: 'LoginCtlr', templateUrl: 'partials/login.html'})
             .when('/logout', {controller: 'LoginCtlr', templateUrl: 'partials/logout.html'})
             .when('/welcome', {controller: 'LoginCtlr', templateUrl: 'partials/start.html'})
             .when('/house', {controller: 'HouseCtlr', templateUrl: 'partials/houseSplashScreen.html'})
-            .otherwise({redirectTo: '/'});
+            .otherwise({redirectTo: '/welcome'});
 });
 
 housebook.run(function ($rootScope, $location, AuthSvc) {
