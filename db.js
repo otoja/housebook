cda8eb955e2297c9783e7ae5b89349dd1e501569
Kamila Przychodzeń <kamila.przychodzen@gmail.com>
2016-12-06 14:00:49 +0100

    Database settings for mongolab

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/housebook');
+//mongoose.connect('mongodb://localhost/housebook');
+mongoose.connect('mongodb://shagrin:xdr5CFT!@ds025459.mlab.com:25459/heroku_2sz0krc7');
 
 module.exports = mongoose;
