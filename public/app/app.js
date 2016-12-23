e6539ca51614b5d92aee568b22f6911886c882da
Kamila Przychodzeń <kamila.przychodzen@gmail.com>
2016-12-23 09:20:41 +0100

    check user session

--- public/app/app.js
+++ public/app/app.js
@@ -2,38 +2,42 @@ var housebook = angular.module('housebook', ['ngRoute']);
 
 housebook.config(function ($routeProvider) {
     $routeProvider
-            .when('/welcome', {templateUrl: 'partials/start.html'})
+            .when('/welcome', {
+                controller: 'LoginCtlr',
+                templateUrl: 'partials/start.html',
+                resolve: {
+                    auth: function (AuthSvc, $rootScope, $location) {
+                        if (AuthSvc.sessionIsValid()) {
+                            AuthSvc.getUser().then(function (user) {
+                                $rootScope.user = user;
+                                $location.path("/house");
+                            });
+                        }
+                    }
+                }
+            })
             .when('/register', {controller: 'RegisterCtlr', templateUrl: 'partials/register.html'})
             .when('/login', {controller: 'LoginCtlr', templateUrl: 'partials/login.html'})
             .when('/logout', {controller: 'LoginCtlr', templateUrl: 'partials/logout.html'})
-            .when('/welcome', {controller: 'LoginCtlr', templateUrl: 'partials/start.html'})
             .when('/house/create', {controller: 'HouseCtlr', templateUrl: 'partials/createHouseProfile.html'})
-            .when('/house/:id', {constroller: 'HouseCtlr', templateUrl: 'partials/houseProfile.html'})
-            .when('/house', {controller: 'HouseCtlr', templateUrl: 'partials/houseSplashScreen.html'})
+            .when('/house/:id', {constroller: 'HouseCtlr', templateUrl: 'partials/houseProfile.html', resolve: {
+                    auth: function (AuthSvc, $rootScope, $location) {
+                        if (AuthSvc.sessionIsValid()) {
+                            AuthSvc.getUser().then(function (user) {
+                                $rootScope.user = user;
+                            });
+                        }
+                    }
+                }})
+            .when('/house', {controller: 'HouseCtlr', templateUrl: 'partials/houseSplashScreen.html', resolve: {
+                    auth: function (AuthSvc, $rootScope, $location) {
+                        if (AuthSvc.sessionIsValid()) {
+                            AuthSvc.getUser().then(function (user) {
+                                $rootScope.user = user;
+                                $location.path("/house");
+                            });
+                        }
+                    }
+                }})
             .otherwise({redirectTo: '/welcome'});
-});
-
-housebook.run(function ($rootScope, $timeout, $location, AuthSvc) {
-    $rootScope.$on("$locationChangeStart", function (event, next, current) {
-        if (AuthSvc.sessionIsValid()) {
-            _.defer(function () {
-                $rootScope.user = AuthSvc.getUser().$$state.value;
-                var location = next.split("#");
-                if (location[1] && (location[1] === "/") || location[1] === "/welcome")
-                    $location.path("/house");
-            });
-        }
-    });
-
-    $rootScope.$on('$viewContentLoaded', function () {
-        if (AuthSvc.sessionIsValid()) {
-            _.defer(function () {
-                $timeout(function () {
-                    $rootScope.user = AuthSvc.getUser().$$state.value;
-                    $rootScope.$apply();
-                });
-
-            });
-        }
-    });
 });
