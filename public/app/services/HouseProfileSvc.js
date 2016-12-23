919fc648eea913ac5bc539a114e380e00a18388b
Kamila Przychodzeń <kamila.przychodzen@gmail.com>
2016-12-23 09:16:25 +0100

    HOUSBOOK-2 create profile

--- public/app/services/HouseProfileSvc.js
+++ public/app/services/HouseProfileSvc.js
@@ -1,34 +1,56 @@
 housebook.service('HouseProfileSvc', function ($q, $http) {
     var obj = {};
+    
     obj.getProfile = function (profileId) {
         var defer = $q.defer();
         $http.get('/house/' + profileId, {headers: {'x-auth': localStorage.getItem('token')}}).then(function (response) {
-            $http.get('/img/' + profileId + '/thumbnail.png')
-                    .success(function () {
-                        response.data.thumbnail = '/img/' + profileId + '/thumbnail.png';
-                        defer.resolve(response.data);
-                    })
-                    .error(function () {
-                        response.data.thumbnail = '/img/test.jpg';
-                        defer.resolve(response.data);
-                    });
+           defer.resolve(response.data);
         });
 
         return defer.promise;
     };
-    obj.saveProfilePicture = function (profileId, thumbnailData, picture) {
-        var data = {thumbnail: thumbnailData};
-        return $http({
-            method: 'POST',
-            url: '/house/' + profileId,
-            data: $.param(data),
-            headers: {'x-auth': localStorage.getItem('token'), 'Content-Type': 'application/x-www-form-urlencoded'}
+    
+    obj.savePicture = function (profileId, name, type, file, isProfilePicture, createdBy) {
+        var defer = $q.defer();
+
+        var data = {profileId: profileId, fileName: name, fileType: type};
+        $http({
+            method: 'GET',
+            url: '/aws/sign-s3',
+            params: data,
+            headers: {'x-auth': localStorage.getItem('token')}
+        }).then(function (response) {
+            var fd = new FormData();
+            fd.append("file", file);
+
+            $http({
+                method: 'PUT',
+                url: response.data.signedRequest,
+                data: file,
+                headers: {'Content-type': file.type}
+            }).then(function (uploadResponse) {
+
+                var payload = {fileName: name, contentType: type, isProfilePicture: isProfilePicture, userId: createdBy};
+
+                $http.post('/house/' + profileId, JSON.stringify(payload), {headers: {'x-auth': localStorage.getItem('token')}});
+                defer.resolve(uploadResponse);
+
+            }, function (err) {
+                defer.reject(err);
+            });
+
+        }, function (err) {
+            defer.reject(err);
         });
+
+        return defer.promise;
     };
 
     obj.createProfile = function (profile) {
         return $http.post('/house/', JSON.stringify(profile), {headers: {'x-auth': localStorage.getItem('token')}});
     };
+
+
 
     return obj;
 });
