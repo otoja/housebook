8ee8be8b3b8b617fda8206df37efe7dcbcfd6971
Kamila Przychodzeń <kamila.przychodzen@gmail.com>
2016-12-27 11:19:48 +0100

    HOUSEBOOK-26 Env specific database

--- db.js
+++ db.js
@@ -3,4 +3,10 @@ var mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost/housebook');
 mongoose.connect(process.env.MONGODB_URI);
 
+mongoose.connection.on('error',function (err) {  
+  console.log('Mongoose default connection error: ' + err);
+});
+mongoose.connection.on('connected', function () {  
+  console.log('Mongoose default connection open to ' + process.env.MONGODB_URI);
+}); 
 module.exports = mongoose;
