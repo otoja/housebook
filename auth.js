3f4e1dc12e08f7bb95eaab6becf3368605c8d50b
Kamila Przychodzeń <kamila.przychodzen@gmail.com>
2016-12-28 17:32:50 +0100

    Check for empty token

--- auth.js
+++ auth.js
@@ -1,11 +1,11 @@
 var jwt = require('jwt-simple');
 var config = require('./config');
 
-module.exports=function(req, res, next){
-  if (req.headers['x-auth']){
-      console.log("x-auth token in auth.js");
-      req.auth = jwt.decode(req.headers['x-auth'], config.secret);
-      console.log(req.auth);
-  }  
-  next();
+module.exports = function (req, res, next) {
+    if (req.headers['x-auth'] && req.headers['x-auth'] !== "null" && req.headers['x-auth'].length>4) {
+        console.log("x-auth token in auth.js" + req.headers['x-auth'].length);
+        req.auth = jwt.decode(req.headers['x-auth'], config.secret);
+        console.log(req.auth);
+    }
+    next();
 };
