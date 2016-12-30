5cf39313b1284374eebb52cb774c6ded197ca30f
Kamila Przychodzeń <kamila.przychodzen@gmail.com>
2016-12-30 16:34:05 +0100

    HOUSEBOOK-3 image manipulationHOUSEBOOK-11 HOUEBOOK-33 HOUSEBOOK-18 mailing system mved to background job

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ module.exports = {
     google_api: "AIzaSyBK5uabiBpR15vbeg-DYFZq9GuQsQNImWY",
     mail_settings: {
         contact_mail: "contact@housebook.info",
-        username: "housebook2",
-        password: "c2W)yChH",
-        smpt_server: "smtp.domeneshop.no",
-        port:587
+        username: "housebook.proxy",
+        password: "Wvs-WHt-D6p-DwM",//"c2W)yChH",
+        smpt_server: 'smtp.gmail.com',//"smtp.domeneshop.no",
+        port:465//587
     }
 
 };
