328f536af39ae7d4504e3ba7127e23ab8cdac878
Kamila Przychodzeń <kamila.przychodzen@gmail.com>
2017-01-04 00:33:16 +0100

    VC-5 Front end page to course detailsvalidation

--- model/address.js
+++ model/address.js
@@ -3,7 +3,7 @@ var db = require('../db');
 var address = db.Schema({
     street1: {type: String, required: false},
     street2: {type: String, required: false},
-    postalCode: {type: Number, required: false},
+    postalCode: {type: String, required: false},
     city: {type: String, required: false},
     country: {type: String, required: false},
     state: {type: String, required: false}
